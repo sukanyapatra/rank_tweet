b49e2da73be88e931c101466e0153046a1e6aaa7
SUKANYA PATRA <sukanyapatra955@gmail.com>
2016-10-22 01:38:21 +0530

    Update sort.py

--- sort.py
+++ sort.py
@@ -1,13 +1,13 @@
 import csv
 import operator
 
-file1 = open('database1.csv','r')
+file1 = open('filtered_tweets.csv','r')
 csv1 = csv.reader(file1)
 
 sort = sorted(csv1,key=operator.itemgetter(0))
 
 for line in sort:
-	with open("database3.csv","a") as file2:
+	with open("database.csv","a") as file2:
 		file2_writer = csv.writer(file2)
 		file2_writer.writerow(line)
 
